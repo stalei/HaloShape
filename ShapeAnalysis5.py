6d6b5dfd7920e8356f6fcd595e29a226459cc01e
Shahram Talei <stalei@crimson.ua.edu>
2019-10-17 19:01:35 -0500

    switched to rockstar halo finder

--- ShapeAnalysis5.py
+++ ShapeAnalysis5.py
@@ -31,13 +31,15 @@ unit_base1 = {'length': (1.0, 'Mpc'),
     'velocity': (1.0, 'km/s'),
     'mass': (1.0e10, 'Msun')}
 
-def _IsContaminated(halo,ds, Rv):#snap):
+def IsContaminated(h,ds):#snap):
 	#halo
-	dds = halo.halo_catalog.data_ds
+	#dds = halo.halo_catalog.data_ds
 	#print(halo.quantities[""])
-	center = dds.arr([halo.quantities["particle_position_%s" % axis] \
-	for axis in "xyz"])
-	my_id = halo.quantities['particle_identifier']
+	center =h.pos# dds.arr([halo.quantities["particle_position_%s" % axis] \
+	Rv=h.R
+	#print(h.R)
+	#for axis in "xyz"])
+	#my_id = halo.quantities['particle_identifier']
 	#center*=3.24078e-25
 	#print("Halo %d" % (my_id))
 	#paarticles
@@ -85,19 +87,26 @@ def _IsContaminated(halo,ds, Rv):#snap):
 
 	#print(len(xLR7))
 	#count=len(xLR7)
-	r2=((xLR4-center[0])**2.+(yLR4-center[1])**2.+(zLR4-center[2])**2.)
+	r2=((xLR4.v-center[0])**2.+(yLR4.v-center[1])**2.+(zLR4.v-center[2])**2.)
 	r=np.sqrt(r2)
 	print("separations in contaminations:")
 	print(r)
 	# should be Rv instead of 0.17 but I have to check their unit and convert them.
 	rContamination=r[r<(Rv)]
 	count=len(rContamination)
-	print("Halo %d at:" % (my_id))
+	print("Halo %d at:" % (h.id))
 	#print(center)
 	print("is contaminated with: %d low resolution particles at δr="%count)
 	print(rContamination)
 	return count
 
+class halo:
+    def __init__(self,pos,R,M,pnum,id):
+        self.pos=pos
+        self.R=R
+        self.M=M
+        self.pnum=pnum
+        self.id=id
 class ellipsoid:
     def __init__(self,axis,orientations):#put in tuple (self,p,angle)..p.x.
         self.a=axis[0]
@@ -111,18 +120,21 @@ class ellipsoid:
     def IsInside(self, point,R):# put in tuple p ... p.x
         #r = R.from_euler('zyx', [self.A, self.B, self.C], degrees=True)
         #point=r.apply(point)
+        #print("point in is inside:")
+        #print(point)
+        #print(R)
         t=0
         pNew=np.array([0.0,0.0,0.0])
         for i in range(0,3):
             for j in range(0,3):
                 #print(point[j])
                 #print(self.orientations[j,i])
-                pNew+=point[j].v*self.orientations[j,i] # each eig vec is [:,i] comp
+                pNew[i]+=point[j]*self.orientations[j,i] # each eig vec is [:,i] comp
         #x=point[0]#-center[0]
         #y=point[1]#-center[1]
         #z=point[2]#-center[2]
         #print("point in is inside:")
-        #print(point)
+        #print(pNew)
         #t=np.sum((pNew/self.axis)**2.)
         for i in range(0,3):
             t+=(pNew[i]/self.axis[i])**2.
@@ -134,9 +146,6 @@ class ellipsoid:
             return False
     def rotate(self, orientation):
         self.orientations=oriantation
-        self.A=EulerAngles[0]
-        self.B=EulerAngles[1]
-        self.C=EulerAngles[2]
     def volume(self):
         return self.axis[0]*self.axis[1]*self.axis[2]
     #    eulerrotation
@@ -148,17 +157,20 @@ class ellipsoid:
         shape=[[0,0,0],[0,0,0],[0,0,0]]#np.array([[0,0,0,],[0,0,0,],[0,0,0]])
         s=0
         #points=coords[(coord[not IsInside()]) && ]
-
         #for i in range(0,3):
         #    p[i]=coords[:,i]
-        rEll=1#np.sqrt(c)Zemp 10
+        #rEll=1#np.sqrt(c)Zemp 10
         #we can use IsInside to find both inside and outside
         for i in range(0,3):
             for j in range(0,3):
                 c=s=0
                 print("s[%d,%d]"%(i,j))
                 for point in coords:
+                    #print("point before if:")
+                    #print(point)
                     if(self.IsInside(point,Rout)):# and not(self.IsInside(point,Rin))):
+                        #print("point after if:")
+                        #print(point)
                         #print("point:%g"%(m*point[i]*point[j]))
                         s+=point[i]*point[j]
                         c+=1
@@ -171,45 +183,53 @@ class ellipsoid:
 def CompareEllipsoids(ell1,ell2):
     AreSame=False;
     error=1.0e-3
+    c=0
     inP=np.array([0,0,0])
     dRatio=np.array([0,0])
     #dax=np.abs(np.divide(ell1.axis/ell2.axis))
     for i in range(1,3):
-        dRatio[i-1]=np.abs((ell1.axis[i]/ell1.axis[0])/(ell2.axis[i]/ell2.axis[0]))
+        if(ell1.axis[0] !=0):
+            dRatio[i-1]=np.abs((ell1.axis[i]/ell1.axis[0])/(ell2.axis[i]/ell2.axis[0]))
+        #dRatio[i-1]=0
     #dori=np.abs(np.divide(ell1.orientations/ell2.orientations))
-    d=np.sum(dRatio[dRatio<1+error & dRatio>1-error])
+    d=np.sum(dRatio[(dRatio<1+error) & (dRatio>1-error)])
     if(d==0):
         for i in range(0,3):
             for j in range(0,3):
                 inP[i]+=ell1.orientations[j,i]*ell2.orientations[j,i]
-            if(inP[i]<((ell1.axis(i))**2.+error) and inP[i]>((ell1.axis(i))**2.-error)):
+            if(inP[i]<((ell1.axis[i])**2.+error) and inP[i]>((ell1.axis[i])**2.-error)):
                 c+=1
         if(c==3):
             AreSame=True
     return AreSame;
-def _GetShape(halo,ds,Rvir):
+def GetShape(h,ds):
     ad = ds.all_data()
     coordinatesDM = ad[("Halo","Coordinates")]
-    dds = halo.halo_catalog.data_ds
-    center = dds.arr([halo.quantities["particle_position_%s" % axis] \
-    for axis in "xyz"])
+    #dds = halo.halo_catalog.data_ds
+    #center = dds.arr([halo.quantities["particle_position_%s" % axis] \
+    #for axis in "xyz"])
     c2=r2=0
     #center*=3.24078e-25
-    center=center.in_units('Mpc')
+    #center=center.in_units('Mpc')
+    center =h.pos# dds.arr([halo.quantities["particle_position_%s" % axis] \
+    Rvir=h.R
     print("halo center (Mpc):")
     print(center)
-    print(coordinatesDM)
+    coordsDM=np.array(coordinatesDM.v)
     for i in range(0,3):
-        coordinatesDM[:,i]-=center[i]
-        print(i)
-    print(coordinatesDM)
+        print(coordsDM[:,i])
+        print(center[i])
+        coordsDM[:,i]-=center[i]
+        print(coordsDM[:,i])
+        #print(i)
+    #print(coordinatesDM)
     for i in range(0,3):
-        r2+=(coordinatesDM[:,i])**2.
+        r2+=(coordsDM[:,i])**2.
         #c2+=(center[i])**2.
     r=np.sqrt(r2)
     print(r)
     #coords=coordinatesDM[np.abs(r-np.sqrt(c2)<Rvir)]
-    coords=coordinatesDM[r<=Rvir]
+    coords=coordsDM[r<=Rvir]
     print("# of virialized particles:%d"%len(coords))
     print(coords)
     #coords[:,0]-=center[0]
@@ -220,33 +240,33 @@ def _GetShape(halo,ds,Rvir):
     print("Individual particle mass: %g"%Pmass[0])
     IDsDM = ad[("Halo","ParticleIDs")]
     print(len(IDsDM))
-    hid = halo.quantities['particle_identifier']
+    hid =h.id# halo.quantities['particle_identifier']
     #halos = HaloFinder(ds, ptype=Halo, dm_only=False, total_mass=None)
     #ind = halos[0]["particle_index"] # list of particles IDs in this halo
     #print(len(ind))
     # REMOVE
     #Rvir=10
 	# Rem
-    bins=1.
+    bins=2.
     iteLim=3
     #convLim=5 nor need, we just compare two
     #Rbins=np.logspace(0,Rvir,bins)#(Rvir/bins,Rvir,bins)
     Rbins=np.linspace(0,Rvir,bins+1)
     print("Rbins:")
     print(Rbins)
-    axis=np.array([Rbins[0],Rbins[0],Rbins[0]])
+    axis=np.array([Rbins[1],Rbins[1],Rbins[1]])
     orientation=np.identity(3)#zeros((3,3))#.array([0,0,0],[0,0,0],[0,0,0])
     a=[0]*len(Rbins)
     b=[0]*len(Rbins)
     c=[0]*len(Rbins)
     b_a=[0]*len(Rbins)
     c_a=[0]*len(Rbins)
-    for i in range(0,len(Rbins)):
+    for i in range(0,len(Rbins)-1):
         iteration=0
         convergence=False
         conv=0
         while(not(convergence) and iteration<iteLim):
-            s=np.array([[0,0,0,],[0,0,0,],[0,0,0]])
+            s=np.array([[1,0,0,],[0,1,0,],[0,0,1]])
             print("halo %d -bin %d -iteration %d"% (hid,i+1, iteration+1))
             ell=ellipsoid(axis,orientation)
             s=ell.ShapeTesnsor(coords,Pmass[0],Rbins[i],Rbins[i+1])
@@ -295,15 +315,16 @@ def _GetShape(halo,ds,Rvir):
 
 
 
-#python ShapeAnalysis.py snapshot_file check_contamination
-#python ShapeAnalysis.py snap_264 1
+#how to run: python ShapeAnalysis.py snapshot_file halo_catalog check_contamination
+#example: $python ShapeAnalysis.py snap_264 halos_0.0.bin 1
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("snap", type=str)
+	parser.add_argument("halo",type=str)
 	parser.add_argument("contamination", type=int)
 	args = parser.parse_args()
-	ds = yt.load(args.snap, unit_base=unit_base1,unit_system='galactic')
+	ds = yt.load(args.snap)#, unit_base=unit_base1)#,unit_system='galactic')
 	if ds is None:
 		print ("Error, sorry, I couldn't read the snapshot.!")
 		sys.exit(1)
@@ -311,30 +332,71 @@ if __name__ == "__main__":
 	print("Time unit: ", ds.time_unit.in_units('Gyr'))
 	print("Mass unit: ", ds.mass_unit.in_units('Msun'))
 	print("Velocity unit: ", ds.velocity_unit.in_units('km/s'))
-	hc = HaloCatalog(data_ds=ds, finder_method='hop', finder_kwargs={ "dm_only": False , "ptype":"Halo"} )
-	#hc = HaloCatalog(data_ds=ds, finder_method='rockstar', finder_kwargs={ "dm_only": False })#, "dm_type":1} )
+	#dh=yt.load(args.halo)
+	dh=np.genfromtxt('halos_0.0.ascii', skip_header=18)
+	if dh is None:
+		print ("Error, sorry, I couldn't read the halo binary file.!")
+		sys.exit(1)
+	#halos = dh.all_data()
+	#hc = HaloCatalog(data_ds=ds, finder_method='hop', finder_kwargs={ "dm_only": False , "ptype":"Halo"} )
+	#hc = HaloCatalog(data_ds=ds, halos_ds=dh)#, finder_method='rockstar')#, finder_kwargs={ "dm_only": False })#, "dm_type":1} )
 	#hc.add_callback("virial_quantities", ["radius"], profile_storage = "virial_quantities_profiles")
 	#hc.add_recipe("calculate_virial_quantities", ["radius"])
-	hc.create(save_halos=True)
-
-	ad0 = hc.halos_ds.all_data()
-	masses = ad0['particle_mass'][:].in_units('Msun')
-	Rvir = ad0['virial_radius'][:]
-	Rvir=Rvir.in_units('Mpc')#*=3.24078e-25#=0.2
+	#hc.create(save_halos=True)
+	#ad0 = hc.halos_ds.all_data()
+	#masses = ad0['particle_mass'][:]#.in_units('Msun')
+	#Rvir = ad0['virial_radius'][:]
+	#Rvir=Rvir.in_units('Mpc')#*=3.24078e-25#=0.2
+	#R2=halos["halos", "virial_radius"]
 	#Rvir=0.25#175
-	print("Mvir=%g-Rvir:%g"%(masses,Rvir))
+	#print("Mvir=%g-Rvir:%g"%(masses,Rvir))
+	Idh=np.array(dh[:,0])
+	#CountAll= len(id)
+	p=1000
+	pnumh=np.array(dh[:,1])
+	Mvirh=np.array(dh[:,2])
+	Rvirh=np.array(dh[:,4])# in kpc
+	xhalo=np.array(dh[:,8])
+	yhalo=np.array(dh[:,9])
+	zhalo=np.array(dh[:,10])
+	#Posh=(xhalo,yhalo,zhalo)
+	#print(Posh)
+
+	print("Mvir-Rvir:")
+	print(Mvirh)
+	print(Rvirh)
+	print(xhalo)
+	print(pnumh)
+	hid=Idh[pnumh>p]
+	hM=Mvirh[pnumh>p]
+	hx=xhalo[pnumh>p]
+	hy=yhalo[pnumh>p]
+	hz=zhalo[pnumh>p]
+	hR=Rvirh[pnumh>p]
+	hR/=1000 # convert to Mpc
+	#print(hR)
+	hP=pnumh[pnumh>p]
+	print(hR)
+	center=np.zeros(3)
+	center[0]=hx
+	center[1]=hy
+	center[2]=hz
+	h=halo(center,hR,hM,hP,hid) # be careful later if you have more than one halo
 	# check for contamination
 	if(args.contamination == 1):
 		print("checking for contamination")
-		add_callback("IsContaminated", _IsContaminated)
-		hc.add_callback("IsContaminated", ds, Rvir)
+		if(not(IsContaminated(h,ds))):
+			print("halo is not contaminated")
+		#add_callback("IsContaminated", _IsContaminated)
+		#hc.add_callback("IsContaminated", ds, Rvir)
 	#hc.create(save_halos=True)
 	#
 	#
 	print("Let's extract the shape!")
-	add_callback("GetShape", _GetShape)
-	hc.add_callback("GetShape",ds,Rvir)
-	hc.create(save_halos=True)
+	GetShape(h,ds)
+	#add_callback("GetShape", _GetShape)
+	#hc.add_callback("GetShape",ds,Rvir)
+	#hc.create(save_halos=True)
         #ad = ds.all_data()
     	#coordinatesDM = ad[("Halo","Coordinates")]
 	#ellipsoid=a,b,c and rotation
