9211d7e22cf32b8d25ea2b4d6bec78f702630af2
Shahram Talei <stalei@crimson.ua.edu>
2021-06-06 16:12:22 -0500

    comparison double plot

---
+++ COM.py
@@ -0,0 +1,69 @@
+#  © Shahram Talei @ 2021 The University of Alabama - All rights reserved.
+#you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from __future__ import division
+import yt
+import numpy as np
+from yt.analysis_modules.halo_finding.api import *
+from yt.analysis_modules.halo_analysis.api import *
+from os import environ
+environ['CFLAGS'] = "-I"+np.get_include()
+
+import pyximport; pyximport.install()
+#import particle_ops
+import argparse
+
+
+import tempfile
+import shutil
+import os
+import sys
+
+from scipy.spatial.transform import Rotation as R
+from numpy import linalg as LA
+from operator import mul
+from functools import reduce
+import matplotlib.pyplot as plt
+
+import csv
+
+plt.rcParams["font.size"] =12
+
+    #how to run: python ShapeAnalysis.py snapshot_file halo_catalog particles_list check_contamination extract_shape bin_number iteration
+    #example: $python ShapeAnalysisV2.py snap_264 halos_0.0.ascii halos_0.0.particles 1.4e12 1.1e12   1 1 5 3
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("snap", type=str)
+    args = parser.parse_args()
+    snap = yt.load(args.snap)
+    ad = snap.all_data()
+    coordinatesDM = ad[("Halo","Coordinates")]
+    p=np.array(coordinatesDM)
+    #print(p[:,1].shape)
+    #print(p[1:])
+    x0=50.9522
+    y0=53.1411
+    z0=47.4905
+    Rv=0.15 #200kpc
+    px=p[:,0]
+    py=p[:,1]
+    pz=p[:,2]
+    dx=px-x0
+    dy=py-y0
+    dz=pz-z0
+    r2=dx*dx+dy*dy+dz*dz
+    r=np.sqrt(r2)
+    x=px[r<Rv]
+    y=py[r<Rv]
+    z=pz[r<Rv]
+    print(len(x))
+    com_x=np.sum(x)/len(x)
+    com_y=np.sum(y)/len(y)
+    com_z=np.sum(z)/len(z)
+    print("COM (x,y,z):%g,%g,%g"%(com_x,com_y,com_z))
--- PlotShapes.py
+++ PlotShapes.py
@@ -8,6 +8,8 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.pyplot import text
+import statistics
 plt.rcParams["font.size"] =12
 #files
 #redshift 0
@@ -28,14 +30,29 @@ f10='/media/shahram/SD/Sample100Mpc/19880/z3/2679.0-C19880z3.csv'
 
 f11='/media/shahram/SD/Sample100Mpc/32251/z3/873.0-G32251z3.csv'
 f12='/media/shahram/SD/Sample100Mpc/32251/z3/852.0-C32251z3.csv'
+
+f13='/media/shahram/SD/Sample100Mpc/32251/High5M/9678.0.G.csv'
+
+f15='/media/shahram/SD/Sample100Mpc/32251/High5M/z1.5/21162.0-G.csv'
+f16='/media/shahram/SD/Sample100Mpc/32251/High5M/z1.5/22045.0-C.csv'
+
+f17='/media/shahram/SD/Sample100Mpc/32251/High5M/z0.62/14003.0-G-30bin.csv'
+f18='/media/shahram/SD/Sample100Mpc/32251/High5M/z0.62/15486.0-C-30bin.csv'
+
+f19='/media/shahram/SD/Sample100Mpc/m12b/z0/6520-m12bGz0.csv'
+f20='/media/shahram/SD/Sample100Mpc/m12f/z0/10226-m12fz0.csv'
+
+f21='/media/shahram/SD/Sample100Mpc/32251/High5M/z0.18/10096-32251Gz0.18.csv'
+f22='/media/shahram/SD/Sample100Mpc/32251/High5M/z0.18/11607-32251Cz0.18.csv'
+
 #open and plot
 
-with open(f12, newline='') as csvfile:
+with open(f21, newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     next(reader)
     next(reader)
     next(reader)
-    size=8 # I have to add a better way to get this number
+    size=15 # I have to add a better way to get this number
     print("Number of bins:%d"%size)
     #declare variables
     R=[0]*size #for all plots
@@ -68,32 +85,131 @@ with open(f12, newline='') as csvfile:
         CC0[i]=np.around(float(row[13]),3)
         i+=1
         print(row[1])
-    fig1 = plt.figure(figsize=plt.figaspect(1./3.))
+    fig1 = plt.figure(1,figsize=plt.figaspect(1./3.))
     ax11 = fig1.add_subplot(131)
     ax11.set_xlabel('R (kpc)')
     ax11.set_ylabel('Axis Ratio')
     ax11.set_ylim(0,1.2)
     ax11.plot(R,b_a,'r',linestyle='-',label="b/a")#str(h.id))
     ax11.plot(R,c_a,'b',linestyle='-',label="c/a")#str(h.id))
-    ax11.plot(R,b_aNoSub,'r',linestyle='-.',label="b/a-No Subhalo")#str(h.id))
-    ax11.plot(R,c_aNoSub,'b',linestyle='-.',label="c/a-No Subhalo")#str(h.id))
+    #ax11.plot(R,b_aNoSub,'r',linestyle='-.',label="b/a-No Subhalo")#str(h.id))
+    #ax11.plot(R,c_aNoSub,'b',linestyle='-.',label="c/a-No Subhalo")#str(h.id))
     ax11.legend(loc=1)
     ax12 = fig1.add_subplot(132)
     ax12.set_xlabel('R (kpc)')
     ax12.set_ylabel('Shape-Angular Momentum')
     ax12.set_ylim(0,1.2)
     ax12.plot(R,AJ,'b',linestyle='-',label="$\hat{A}.\hat{L}$")#+str(h.id))
-    ax12.plot(R,BJ,'r',linestyle='-.',label="$\hat{B}.\hat{L}$")#+str(h.id))
-    ax12.plot(R,CJ,'g',linestyle=':',label="$\hat{C}.\hat{L}$")#+str(h.id))
+    ax12.plot(R,BJ,'r',linestyle='-',label="$\hat{B}.\hat{L}$")#+str(h.id))
+    ax12.plot(R,CJ,'g',linestyle='-',label="$\hat{C}.\hat{L}$")#+str(h.id))
     ax12.legend(loc=2,ncol=3)
     ax13 = fig1.add_subplot(133)
     ax13.set_xlabel('R (kpc)')
     ax13.set_ylabel('Residual Orientation')
     ax13.set_ylim(0,1.2)
     ax13.plot(R,AA0,'b',linestyle='-',label="$\hat{A}.\hat{A}_0$")#+str(h.id))
-    ax13.plot(R,BB0,'r',linestyle='-.',label="$\hat{B}.\hat{B}_0$")#+str(h.id))
-    ax13.plot(R,CC0,'g',linestyle=':',label="$\hat{C}.\hat{C}_0$")#+str(h.id))
+    ax13.plot(R,BB0,'r',linestyle='-',label="$\hat{B}.\hat{B}_0$")#+str(h.id))
+    ax13.plot(R,CC0,'g',linestyle='-',label="$\hat{C}.\hat{C}_0$")#+str(h.id))
     ax13.legend(loc=3)
     print(R)
     print(b_a)
+    fig2=plt.figure(2,figsize=plt.figaspect(1.))
+    ax21 = fig2.add_subplot(111)
+    ax21.set_xlabel('b/a')
+    ax21.set_ylabel('c/a')
+    ax21.scatter(b_a,c_a,marker='o',color='b',label='Gadget')
+    #ax21.plot(b_a,b_a,'r',linestyle='-',label="Prolate")#str(h.id))
+    text(0.4,0.5 , "Prolate", rotation=45, verticalalignment='center')
+    text(0.77,0.4 , "Oblate", rotation=90, verticalalignment='center')
+    text(0.63,0.78 , "Sphere", rotation=0, verticalalignment='center')
+    b_a_ave=statistics.mean(b_a)
+    c_a_ave=statistics.mean(c_a)
+    b_a_std=np.std(b_a)
+    c_a_std=np.std(c_a)
+    ax21.errorbar(b_a_ave, c_a_ave, b_a_std,c_a_std, linestyle='None', marker='x',label='Gadget')
+    #plt.show()
+####
+# 2nd plot
+
+with open(f22, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    next(reader)
+    next(reader)
+    next(reader)
+    size=15 # I have to add a better way to get this number
+    print("Number of bins:%d"%size)
+    #declare variables
+    R=[0]*size #for all plots
+    #sub 131
+    b_a=[0]*size
+    b_aNoSub=[0]*size
+    c_a=[0]*size
+    c_aNoSub=[0]*size
+    #sub 132
+    AJ=[0]*size
+    BJ=[0]*size
+    CJ=[0]*size
+    #sub 133
+    AA0=[0.0]*size
+    BB0=[0.0]*size
+    CC0=[0.0]*size
+
+    i=0
+    for row in reader:
+        R[i]=np.around(float(row[1]),3)*1000
+        b_a[i]=np.around(float(row[2]),3)
+        c_a[i]=np.around(float(row[3]),3)
+        b_aNoSub[i]=np.around(float(row[5]),3)
+        c_aNoSub[i]=np.around(float(row[6]),3)
+        AJ[i]=np.around(float(row[8]),3)
+        BJ[i]=np.around(float(row[10]),3)
+        CJ[i]=np.around(float(row[12]),3)
+        AA0[i]=np.around(float(row[9]),3)
+        BB0[i]=np.around(float(row[11]),3)
+        CC0[i]=np.around(float(row[13]),3)
+        i+=1
+        print(row[1])
+    fig1 = plt.figure(1,figsize=plt.figaspect(1./3.))
+    #ax11 = fig1.add_subplot(131)
+    ax11.set_xlabel('R (kpc)')
+    ax11.set_ylabel('Axis Ratio')
+    ax11.set_ylim(0,1.2)
+    ax11.plot(R,b_a,'r',linestyle='-.',label="b/a")#str(h.id))
+    ax11.plot(R,c_a,'b',linestyle='-.',label="c/a")#str(h.id))
+    #ax11.plot(R,b_aNoSub,'r',linestyle='-.',label="b/a-No Subhalo")#str(h.id))
+    #ax11.plot(R,c_aNoSub,'b',linestyle='-.',label="c/a-No Subhalo")#str(h.id))
+    ax11.legend(loc=1)
+    #ax12 = fig1.add_subplot(132)
+    #ax12.set_xlabel('R (kpc)')
+    #ax12.set_ylabel('Shape-Angular Momentum')
+    ax12.set_ylim(0,1.2)
+    ax12.plot(R,AJ,'b',linestyle='-.',label="$\hat{A}.\hat{L}$")#+str(h.id))
+    ax12.plot(R,BJ,'r',linestyle='-.',label="$\hat{B}.\hat{L}$")#+str(h.id))
+    ax12.plot(R,CJ,'g',linestyle='-.',label="$\hat{C}.\hat{L}$")#+str(h.id))
+    ax12.legend(loc=2,ncol=3)
+    #ax13 = fig1.add_subplot(133)
+    #ax13.set_xlabel('R (kpc)')
+    #ax13.set_ylabel('Residual Orientation')
+    ax13.set_ylim(0,1.2)
+    ax13.plot(R,AA0,'b',linestyle='-.',label="$\hat{A}.\hat{A}_0$")#+str(h.id))
+    ax13.plot(R,BB0,'r',linestyle='-.',label="$\hat{B}.\hat{B}_0$")#+str(h.id))
+    ax13.plot(R,CC0,'g',linestyle='-.',label="$\hat{C}.\hat{C}_0$")#+str(h.id))
+    ax13.legend(loc=3)
+    print(R)
+    print(b_a)
+    fig2=plt.figure(2,figsize=plt.figaspect(1.))
+    #ax21 = fig2.add_subplot(111)
+    ax21.set_xlabel('b/a')
+    ax21.set_ylabel('c/a')
+    ax21.scatter(b_a,c_a,marker='s',color='r',label='CoSANG')
+    ax21.plot(b_a,b_a,'k',linestyle='-')#str(h.id))
+    text(0.4,0.5 , "Prolate", rotation=45, verticalalignment='center')
+    text(0.77,0.4 , "Oblate", rotation=90, verticalalignment='center')
+    text(0.63,0.78 , "Sphere", rotation=0, verticalalignment='center')
+    b_a_ave=statistics.mean(b_a)
+    c_a_ave=statistics.mean(c_a)
+    b_a_std=np.std(b_a)
+    c_a_std=np.std(c_a)
+    ax21.errorbar(b_a_ave, c_a_ave, b_a_std,c_a_std, linestyle='None', marker='x',label='CoSANG')
+    ax21.legend(loc=2)
     plt.show()
